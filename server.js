4661f759fb761569665d83ae6cffc280b07bb537
Sam <mirthquakes@gmail.com>
2014-09-13 22:10:25 -0500

    add app skeleton

---
+++ server.js
@@ -0,0 +1,81 @@
+#!/bin/env node
+var express = require('express');
+var fs      = require('fs');
+
+var TransitApp = function() {
+
+    var self = this;
+
+    self.setupVariables = function() {
+        //  Set the environment variables we need.
+        self.ipaddress = process.env.OPENSHIFT_NODEJS_IP;
+        self.port      = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+
+        if (typeof self.ipaddress === "undefined") {
+            console.warn('No OPENSHIFT_NODEJS_IP var, using 127.0.0.1');
+            self.ipaddress = "127.0.0.1";
+        };
+    };
+
+    self.terminator = function(sig){
+        if (typeof sig === "string") {
+           console.log('%s: Received %s - terminating sample app ...',
+                       Date(Date.now()), sig);
+           process.exit(1);
+        }
+        console.log('%s: Node server stopped.', Date(Date.now()) );
+    };
+
+    self.setupTerminationHandlers = function(){
+        //  Process on exit and signals.
+        process.on('exit', function() { self.terminator(); });
+
+        // Removed 'SIGPIPE' from the list - bugz 852598.
+        ['SIGHUP', 'SIGINT', 'SIGQUIT', 'SIGILL', 'SIGTRAP', 'SIGABRT',
+         'SIGBUS', 'SIGFPE', 'SIGUSR1', 'SIGSEGV', 'SIGUSR2', 'SIGTERM'
+        ].forEach(function(element, index, array) {
+            process.on(element, function() { self.terminator(element); });
+        });
+    };
+
+    self.createRoutes = function() {
+        self.app.get('/', function(req, res) {
+			res.send("hello world");
+        });
+    };
+
+
+    self.initializeServer = function() {
+        self.app = express();
+		self.app.configure(function(){
+			self.app.use(express.json());
+			self.app.use(express.urlencoded());
+			self.app.use(self.app.router);
+			
+			self.app.use(express.static(__dirname));
+		});
+		
+		
+		self.createRoutes();
+     
+    };
+
+    self.initialize = function() {
+        self.setupVariables();
+        self.setupTerminationHandlers();
+        self.initializeServer();
+    };
+
+    self.start = function() {
+        self.app.listen(self.port, self.ipaddress, function() {
+            console.log('%s: Node server started on %s:%d ...',
+                        Date(Date.now() ), self.ipaddress, self.port);
+        });
+    };
+
+}; 
+
+var zapp = new TransitApp();
+zapp.initialize();
+zapp.start();
+
