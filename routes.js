a9b1799418d0367c4a36b900544ad5fb7332f828
Sam <mirthquakes@gmail.com>
2014-09-14 14:36:23 -0500

    add GTFSs models to my solution and install mongoose

--- routes.js
+++ routes.js
@@ -1,5 +1,39 @@
+var mongoose = require('mongoose');
+var db = mongoose.connect('mongodb://localhost:27017/gtfs');
+
+require('./models/Agency');
+require('./models/Calendar');
+require('./models/CalendarDate');
+require('./models/FareAttribute');
+require('./models/FareRule');
+require('./models/FeedInfo');
+require('./models/Frequencies');
+require('./models/Route');
+require('./models/Stop');
+require('./models/StopTime');
+require('./models/Transfer');
+require('./models/Trip');
+
+var Agency = db.model('Agency')
+  , Calendar = db.model('Calendar')
+  , Route = db.model('Route')
+  , Stop = db.model('Stop')
+  , StopTime = db.model('StopTime')
+  , Trip = db.model('Trip')
+  , Calendar = db.model('Calendar');
+
+var agencyKey = 'birmingham-jefferson-county-transit-authority';
+
+
 module.exports = function routes(app){
 	app.get('/', function(req, res) {
-		res.send("hello world!");
+		var matchingAgency = Route.find({agency_key:agencyKey}, function(e, data){
+			res.send(data);
+		});
+		
+		
+    });
+	app.get('/stops/:routeID', function(req, res) {
+	
     });
 };
