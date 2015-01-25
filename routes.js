555c308abb31a13deddd303d744af59dbb41d2da
Sam <mirthquakes@gmail.com>
2015-01-25 15:42:13 -0600

    design query for retrieving trip, stop, and time info

--- routes.js
+++ routes.js
@@ -9,6 +9,10 @@ module.exports = function routes(app){
     });
 	
 	app.get('/stops/:routeID', function(req, res) {
-		
+		/*
+		Get all trips, stops, and stop times for a route
+		SELECT TRIPS.SERVICE_ID, TRIPS.TRIP_ID, TRIPS.TRIP_HEADSIGN, STOP_TIMES.ARRIVAL_TIME, STOP_TIMES.STOP_ID, STOPS.STOP_NAME FROM TRIPS INNER JOIN STOP_TIMES ON STOP_TIMES.TRIP_ID=TRIPS.TRIP_ID INNER JOIN STOPS ON STOPS.STOP_ID=STOP_TIMES.STOP_ID WHERE TRIPS.ROUTE_ID='R1'		
+		*/
+
     });
 };
